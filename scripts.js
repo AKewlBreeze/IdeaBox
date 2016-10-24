a07f81e667ae3c229db1824e44ea02888a2dd455
Breana Knight <Akewlbreeze.bc@gmail.com>
2016-10-24 16:04:21 -0600

    1st wave of JSON & JS integration with pseudo code

---
+++ scripts.js
@@ -0,0 +1,23 @@
+var $inputTitle = $(".title")
+
+var $inputBody = $(".description")
+
+$(".save").on("click", function(e){
+  var inputTitle = $inputTitle.val()
+  var inputBody = $inputBody.val()
+  var newIdea = new Idea(inputTitle, inputBody)
+  debugger
+// grab from local storage json parse
+// add new idea to array
+// json stringify to send back to local storage
+// grab ideas from local storage and display on page
+// clear input values
+})
+
+
+function Idea(title, body){
+  this.title = title;
+  this.body = body;
+  this.id = Date.now()
+  this.quality = "swill"
+}
