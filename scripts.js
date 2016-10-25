a16edd056393c52e87cf6ef82f016167f295089f
Stephanie Andrews <seandrews11@gmail.com>
2016-10-25 14:03:54 -0600

    Work in progress

--- scripts.js
+++ scripts.js
@@ -1,17 +1,25 @@
 var $inputTitle = $(".title")
-
 var $inputBody = $(".description")
 
-$(".save").on("click", function(e){
+$(".save").on("click", function(){
   var inputTitle = $inputTitle.val()
   var inputBody = $inputBody.val()
   var newIdea = new Idea(inputTitle, inputBody)
-  debugger
-// grab from local storage json parse
-// add new idea to array
+// create variable of arrays to store
+  var savedIdeas = [];
+//grab from local storage json parse
+  JSON.parse(localStorage.getItem(savedIdeas));
+//add new idea object to saved ideas
+  savedIdeas.push(newIdea);
+// create variable to access key of newIdea from savedIdeas
+  var localStorageKey = savedIdeas[savedIdeas.length-1].id
 // json stringify to send back to local storage
+  JSON.stringify(localStorage.setItem(localStorageKey,savedIdeas));
 // grab ideas from local storage and display on page
+
 // clear input values
+//  $inputTitle.val(null);
+//  $inputBody.val(null);
 })
 
 
