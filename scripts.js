f1d4af94bb6a0b635ce95c66e2047551f3829430
Stephanie Andrews <seandrews11@gmail.com>
2016-10-25 16:06:07 -0600

    Remove array and add steps

--- scripts.js
+++ scripts.js
@@ -5,27 +5,29 @@ $(".save").on("click", function(){
   var inputTitle = $inputTitle.val()
   var inputBody = $inputBody.val()
   var newIdea = new Idea(inputTitle, inputBody)
-// create variable of arrays to store
-  var savedIdeas = [];
-//grab from local storage json parse
-  JSON.parse(localStorage.getItem(savedIdeas));
-//add new idea object to saved ideas
-  savedIdeas.push(newIdea);
-// create variable to access key of newIdea from savedIdeas
-  var localStorageKey = savedIdeas[savedIdeas.length-1].id
+  var localKey = newIdea.id
+
 // json stringify to send back to local storage
-  JSON.stringify(localStorage.setItem(localStorageKey,savedIdeas));
-// grab ideas from local storage and display on page
+  JSON.stringify(localStorage.setItem(localKey, newIdea));
+// json parse to access item from local storage
+//JSON.parse(localStorage.getItem(localKey))
+
+// grab idea from local storage and display on page
+  $('.bottom').appendChild(newIdea)
 
 // clear input values
-//  $inputTitle.val(null);
-//  $inputBody.val(null);
-})
+$inputTitle.val(null);
+$inputBody.val(null);
 
+// evaluate prepend to show the newest ideas first
+})
 
 function Idea(title, body){
   this.title = title;
   this.body = body;
   this.id = Date.now()
   this.quality = "swill"
+  this.creation = document.createElement('article')
+  this.class = 'ideabody'
+  this.innerHTML = '<span class = "delete"><h2>Example Idea 1<img src="./images/delete.svg"/ class = "deleteArrow"></h2></span><p class = "body-content">Example Content</p><p class = "quality"><img src = "./images/upvote.svg" class = "upvote"><img src = "./images/downvote.svg" class = "downvote">quality:</p><hr>'
 }
